fd4c91142e2e260a211fcc436dc59504c114b9a1
Tristan79 <tristan.net@gmail.com>
2019-08-28 00:24:09 +0200

    .

--- x720packages.sh
+++
@@ -1,4 +0,0 @@
-#X720 Download Packages
-apt-get install python3-pip i2c-tools
-pip3 install smbus
-pip3 install paho-mqtt
--- x720rtc.sh
+++
@@ -1,4 +0,0 @@
-#X720 RTC setting up
-sudo sed -i '$ i rtc-ds1307' /etc/modules
-sudo sed -i '$ i echo ds1307 0x68 > /sys/class/i2c-adapter/i2c-1/new_device' /etc/rc.local
-sudo sed -i '$ i hwclock -s' /etc/rc.local
